8a1f5657a5b6256191e9677a715604dfddc85901
Matt Hale <mlhale@unomaha.edu>
2017-05-27 01:37:58 -0500

    added license to prominent socket consumer file

--- api/socketconsumers.py
+++ api/socketconsumers.py
@@ -1,3 +1,26 @@
+###############################################################################
+# GazepointWebsockets: This project converts eye tracker data collected from a
+# Gazepoint GP3 to simple JSON and then sends collected events to an
+# experimentation platform, such as the Cybertrust phishing research platform,
+# using websockets.
+#
+# Author: Matthew L. Hale
+# Email: mlhale@unomaha.edu
+# Copyright (C) 2017 Dr. Matthew L. Hale
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see http://www.gnu.org/licenses/.
+# Based on PyOpenGaze by Edwin Dalmaijer (edwin.dalmaijer@psy.ox.ac.uk)
+###############################################################################
 import os
 import time
 import json
